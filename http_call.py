abd978fef873dec9e6de72eb77bc023c613a1b76
Premal Samale <36308376+PremalSamale@users.noreply.github.com>
2018-09-02 22:55:58 -0700

    improved code with for loop and using URL variable
    SJSU ID: 012566333 (Premal Dattatray Samale)

--- http_call.py
+++ http_call.py
@@ -1,8 +1,10 @@
 import requests
 
-response1 = requests.get('https://webhook.site/d56af0d6-4354-4f3c-9e7d-bd3f87364bc5')
-response2 = requests.get('https://webhook.site/d56af0d6-4354-4f3c-9e7d-bd3f87364bc5')
-response3 = requests.get('https://webhook.site/d56af0d6-4354-4f3c-9e7d-bd3f87364bc5')
-print("Response1:",response1.headers.get('Date'))
-print("Response2:",response2.headers.get('Date'))
-print("Response3:",response3.headers.get('Date'))
+url = 'https://webhook.site/d56af0d6-4354-4f3c-9e7d-bd3f87364bc5'
+response_list = []
+for index in range(0,3):
+    response_list.append(requests.get(url))
+
+print('Response1:', response_list[0].headers.get('Date'))
+print('Response2:', response_list[1].headers.get('Date'))
+print('Response3:', response_list[2].headers.get('Date'))
