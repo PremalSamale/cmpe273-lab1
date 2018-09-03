1ce69dc800d64a5f73e798382f960ffb1143f9fd
Premal Samale <36308376+PremalSamale@users.noreply.github.com>
2018-09-02 22:53:39 -0700

    improved code with for loop and using URL variable
    SJSU ID: 012566333 (Premal Dattatray Samale)

--- http_async_call.py
+++ http_async_call.py
@@ -1,19 +1,19 @@
-
 import asyncio
 import requests
 
 #Reference:http://skipperkongen.dk/2016/09/09/easy-parallel-http-requests-with-python-and-asyncio/
 
+url = 'https://webhook.site/d56af0d6-4354-4f3c-9e7d-bd3f87364bc5'
 async def main():
     loop = asyncio.get_event_loop()
-    result1 = loop.run_in_executor(None, requests.get, 'https://webhook.site/d56af0d6-4354-4f3c-9e7d-bd3f87364bc5')
-    result2 = loop.run_in_executor(None, requests.get, 'https://webhook.site/d56af0d6-4354-4f3c-9e7d-bd3f87364bc5')
-    result3 = loop.run_in_executor(None, requests.get, 'https://webhook.site/d56af0d6-4354-4f3c-9e7d-bd3f87364bc5')
-    response1 = await result1
-    response2 = await result2
-    response3 = await result3
-    print("Response1:",response1.headers.get('Date'))
-    print("Response2:",response2.headers.get('Date'))
-    print("Response3:",response3.headers.get('Date'))
+    results = []
+    for index in range(0,3):
+        results.append(loop.run_in_executor(None, requests.get, url))
+    response = []
+    for index in range(0,3):
+        response.append(await results[index])
+    print("Response1:",response[0].headers.get('Date'))
+    print("Response2:",response[1].headers.get('Date'))
+    print("Response3:",response[2].headers.get('Date'))
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
